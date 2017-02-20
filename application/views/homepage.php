7e54ddbde8f4112f7dfd1684f3c2911f1ac8fbad
jukkapongtor <jukkapong.marsri@gmail.com>
2017-02-20 15:24:30 +0700

    add project ci1 mumfern

---
+++ application/views/homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>ยินดีต้อนรับสู่มุมเฟิร์น</title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/mystyle.css">
+	</head>
+	<body >
+		<div class="container-fluid IndHeader">
+			<div class="container-fluid">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="col-md-3 IndIcon">
+						MUMFERN
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="col-md-8 col-md-offset-2 IndMain">
+				
+			</div>
+		</div>
+		<div class="container-fluid IndFooter">
+
+		</div>
+	</body>
+  </html>
